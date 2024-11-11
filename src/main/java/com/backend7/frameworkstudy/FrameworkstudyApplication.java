73cdc2b1d044edaaf33f3403e3a5cb114d0670bf
박성현 <qkr970504@gmail.com>
2024-11-11 15:19:00 +0900

    init: build project
    - application 환경 분리

---
+++ src/main/java/com/backend7/frameworkstudy/FrameworkstudyApplication.java
@@ -0,0 +1,13 @@
+package com.backend7.frameworkstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrameworkstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend7/frameworkstudy/FrameworkstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend7.frameworkstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
