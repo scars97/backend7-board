d5c3e7b0ab949dc9d176ec8880f9b59fe10b8354
박성현 <qkr970504@gmail.com>
2024-11-12 16:06:53 +0900

    test: 게시글 상세 조회 테스트
    - 게시글 상세 조회 예외 케이스 테스트

--- src/test/java/com/backend7/frameworkstudy/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/backend7/frameworkstudy/domain/board/service/BoardServiceTest.java
@@ -3,13 +3,14 @@ package com.backend7.frameworkstudy.domain.board.service;
 import com.backend7.frameworkstudy.domain.board.domain.Board;
 import com.backend7.frameworkstudy.domain.board.dto.BoardCreateRequest;
 import com.backend7.frameworkstudy.domain.board.dto.BoardResponse;
+import com.backend7.frameworkstudy.domain.board.repository.BoardRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -17,6 +18,8 @@ class BoardServiceTest {
 
     @Autowired
     private BoardService boardService;
+    @Autowired
+    private BoardRepository boardRepository;
 
     @DisplayName("게시글이 정상적으로 저장되고, 작성한 게시글이 반환된다.")
     @Test
@@ -38,6 +41,19 @@ class BoardServiceTest {
         assertThat(response)
                 .extracting("title", "content", "username")
                 .contains("게시글1", "게시글 내용", "test@test.com");
+    }
+
+    @DisplayName("해당 id의 게시글이 존재하지 않는 경우 예외가 발생한다.")
+    @Test
+    void findBoardWithOutId() {
+        // given
+        Board board = create();
+        boardRepository.save(board);
+
+        // when //then
+        assertThatThrownBy(() -> boardService.findBoardBy(2L))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("게시글이 존재하지 않습니다.");
     }
 
     private Board create() {
