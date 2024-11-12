ea133f25f99e46d6d11bdeba4608426d1d18200e
박성현 <qkr970504@gmail.com>
2024-11-12 17:23:43 +0900

    test: 게시글 수정 테스트
    - 비밀번호 일치 여부 확인

--- src/test/java/com/backend7/frameworkstudy/domain/board/service/BoardServiceTest.java
+++ src/test/java/com/backend7/frameworkstudy/domain/board/service/BoardServiceTest.java
@@ -3,7 +3,9 @@ package com.backend7.frameworkstudy.domain.board.service;
 import com.backend7.frameworkstudy.domain.board.domain.Board;
 import com.backend7.frameworkstudy.domain.board.dto.BoardCreateRequest;
 import com.backend7.frameworkstudy.domain.board.dto.BoardResponse;
+import com.backend7.frameworkstudy.domain.board.dto.BoardUpdateRequest;
 import com.backend7.frameworkstudy.domain.board.repository.BoardRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,11 @@ class BoardServiceTest {
     private BoardService boardService;
     @Autowired
     private BoardRepository boardRepository;
+
+    @AfterEach
+    void tearDown() {
+        boardRepository.deleteAllInBatch();
+    }
 
     @DisplayName("게시글이 정상적으로 저장되고, 작성한 게시글이 반환된다.")
     @Test
@@ -54,6 +61,53 @@ class BoardServiceTest {
         assertThatThrownBy(() -> boardService.findBoardBy(2L))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("게시글이 존재하지 않습니다.");
+    }
+
+    @DisplayName("비밀번호 일치여부를 확인하고 게시글을 수정한다.")
+    @Test
+    void editBoardAfterCheckForPasswordMatch() {
+        // given
+        Board saveBoard = boardRepository.save(create());
+
+        Long updateId = saveBoard.getId();
+        BoardUpdateRequest updateRequest = BoardUpdateRequest.builder()
+                .title("게시글123123")
+                .content("게시글 내용123123")
+                .username("test1111@test.com")
+                .password("1234")
+                .build();
+
+        // when
+        Board findBoard = boardRepository.findById(updateId).orElse(saveBoard);
+
+        //then
+        assertThat(findBoard.getPassword()).isEqualTo(updateRequest.getPassword());
+
+        findBoard.update(updateRequest);
+        assertThat(findBoard)
+                .extracting("title", "content", "username")
+                .contains("게시글123123", "게시글 내용123123", "test1111@test.com");
+    }
+
+    @DisplayName("비밀번호가 일치하지 않는 경우 게시글이 수정되지 않고 예외가 발생한다.")
+    @Test
+    void passwordNotMatchThenExceptionThrown() {
+        // given
+        Board saveBoard = boardRepository.save(create());
+
+        Long updateId = saveBoard.getId();
+        BoardUpdateRequest updateRequest = BoardUpdateRequest.builder()
+                .title("게시글123123")
+                .content("게시글 내용123123")
+                .username("test1111@test.com")
+                .password("11111")
+                .build();
+
+        // when //then
+        assertThatThrownBy(() -> boardService.editBoard(updateId, updateRequest))
+                .hasMessage("비밀번호가 일치하지 않습니다.")
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(saveBoard.getTitle()).isNotEqualTo(updateRequest.getTitle());
     }
 
     private Board create() {
