1880d57d57548109e1f10477f48307e036a4bf91
박성현 <qkr970504@gmail.com>
2024-11-17 22:50:42 +0900

    feat: Security cconfig 추가

---
+++ src/main/java/com/backend7/frameworkstudy/global/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.backend7.frameworkstudy.global.config;
+
+import com.backend7.frameworkstudy.domain.auth.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    public static final String[] GET_PUBLIC = new String[] {"/api/board"};
+    public static final String[] POST_PUBLIC = new String[] {"/api/auth/sign-up", "/api/auth/login"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+                auth ->
+                    auth.requestMatchers(HttpMethod.GET, GET_PUBLIC)
+                            .permitAll()
+                        .requestMatchers(HttpMethod.POST, POST_PUBLIC)
+                            .permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
