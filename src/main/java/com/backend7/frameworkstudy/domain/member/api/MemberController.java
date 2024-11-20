017730ddda32e9124741b5e73d5c211686290ee0
박성현 <qkr970504@gmail.com>
2024-11-20 18:44:27 +0900

    feat: 회원 가입 사용자 입력 유효성 검증

--- src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
+++ src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
@@ -1,7 +1,9 @@
 package com.backend7.frameworkstudy.domain.member.api;
 
 import com.backend7.frameworkstudy.domain.member.dto.MemberCreateRequest;
+import com.backend7.frameworkstudy.domain.member.dto.SignUpResponse;
 import com.backend7.frameworkstudy.domain.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<String> singUp(@RequestBody MemberCreateRequest request) {
+    public ResponseEntity<SignUpResponse> singUp(@Valid @RequestBody MemberCreateRequest request) {
         return ResponseEntity.ok(memberService.signUp(request));
     }
 }
--- src/main/java/com/backend7/frameworkstudy/domain/member/dto/MemberCreateRequest.java
+++ src/main/java/com/backend7/frameworkstudy/domain/member/dto/MemberCreateRequest.java
@@ -1,14 +1,26 @@
 package com.backend7.frameworkstudy.domain.member.dto;
 
 import com.backend7.frameworkstudy.domain.member.domain.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class MemberCreateRequest {
 
+    @NotBlank
+    @Size(min = 4, max = 10, message = "최소 4자 이상, 최대 10자 이하여야 합니다.")
+    @Pattern(regexp = "[a-z0-9]+", message = "알파벳 소문자(a~z)와 숫자(0~9)만 가능합니다.")
     private String username;
+
+    @NotBlank
+    @Size(min = 8, max = 15, message = "최소 8자 이상, 최대 15자 이하여야 합니다.")
+    @Pattern(regexp = "[a-zA-z0-9]+", message = "알파벳 대소문(a~z, A~Z)와 숫자(0~9)만 가능합니다.")
     private String password;
 
     public Member toEntity() {
