8e8f64a654c07d6d30cd7598cf1f2682b0295f4b
박성현 <qkr970504@gmail.com>
2024-11-21 21:27:33 +0900

    test: 회원 controller 테스트
    - MockMvcBuilders 통합
    - 회원가입, 로그인 해피 케이스 추가

--- src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
+++ src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
@@ -1,11 +1,15 @@
 package com.backend7.frameworkstudy.domain.member.api;
 
+import com.backend7.frameworkstudy.domain.auth.JwtTokenProvider;
+import com.backend7.frameworkstudy.domain.member.dto.LoginRequest;
 import com.backend7.frameworkstudy.domain.member.dto.MemberCreateRequest;
-import com.backend7.frameworkstudy.domain.member.dto.SignUpResponse;
+import com.backend7.frameworkstudy.domain.member.dto.MemberResponse;
+import com.backend7.frameworkstudy.domain.member.exception.MemberResultType;
 import com.backend7.frameworkstudy.domain.member.service.MemberService;
+import com.backend7.frameworkstudy.global.common.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<SignUpResponse> singUp(@Valid @RequestBody MemberCreateRequest request) {
-        return ResponseEntity.ok(memberService.signUp(request));
+    public ApiResponse<MemberResponse> singUp(@Valid @RequestBody MemberCreateRequest request) {
+        return ApiResponse.ok(MemberResultType.SIGN_UP, memberService.signUp(request));
     }
+
+    @PostMapping("/login")
+    public ApiResponse<MemberResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
+        MemberResponse memberResponse = memberService.loginUser(request);
+
+        // access 토큰 발급을 controller에서 하는 것은 조금 어색하다. - 고민필요
+        response.addHeader("Authorization", "Bearer " + jwtTokenProvider.generateAccessToken(memberResponse.getId()));
+        return ApiResponse.ok(MemberResultType.LOGIN, memberService.loginUser(request));
+    }
+
+    // refresh 토큰을 사용한 access 토큰 재발급
 }
--- src/test/java/com/backend7/frameworkstudy/domain/member/api/MemberControllerTest.java
+++ src/test/java/com/backend7/frameworkstudy/domain/member/api/MemberControllerTest.java
@@ -1,10 +1,11 @@
 package com.backend7.frameworkstudy.domain.member.api;
 
+import com.backend7.frameworkstudy.domain.member.dto.LoginRequest;
 import com.backend7.frameworkstudy.domain.member.dto.MemberCreateRequest;
 import com.backend7.frameworkstudy.domain.member.service.MemberService;
 import com.backend7.frameworkstudy.global.error.GlobalExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,9 +17,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.Map;
-
-import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,13 +36,17 @@ class MemberControllerTest {
 
     private MockMvc mockMvc;
 
+    @BeforeEach
+    public void init() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(memberController)
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
+    }
+
     @DisplayName("유효하지 않은 입력으로 회원 가입 요청 시 검증 실패 메시지를 반환한다.")
     @Test
     void signUp_InvalidInput() throws Exception {
         // given
-        mockMvc = MockMvcBuilders.standaloneSetup(memberController)
-                .setControllerAdvice(new GlobalExceptionHandler())
-                .build();
         MemberCreateRequest invalidRequest = new MemberCreateRequest("te", "12");
 
         // when //then
@@ -58,5 +60,41 @@ class MemberControllerTest {
                 .andExpect(jsonPath("$.reason").isNotEmpty())
                 .andExpect(jsonPath("$.reason.username").value("최소 4자 이상, 최대 10자 이하여야 합니다."))
                 .andExpect(jsonPath("$.reason.password").value("최소 8자 이상, 최대 15자 이하여야 합니다."));
+    }
+
+    @DisplayName("회원 가입에 성공한다.")
+    @Test
+    void signUp() throws Exception {
+        // given
+        MemberCreateRequest request = new MemberCreateRequest("test1234", "qwer1234");
+
+        // when //then
+        mockMvc.perform(post("/api/auth/sign-up")
+                        .content(objectMapper.writeValueAsString(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("회원 가입에 성공하였습니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("로그인에 성공한다.")
+    @Test
+    void loginUser() throws Exception {
+        // given
+        LoginRequest request = new LoginRequest("test1234", "12341234");
+
+        // when //then
+        mockMvc.perform(post("/api/auth/login")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("로그인에 성공하였습니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
     }
 }
