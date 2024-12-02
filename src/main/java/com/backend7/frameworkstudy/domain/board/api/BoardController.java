783a43d812f78c748a2109502d354f3ada81b580
박성현 <qkr970504@gmail.com>
2024-12-02 15:11:08 +0900

    feat: 회원, 게시글 API 명세서 추가

--- src/main/java/com/backend7/frameworkstudy/domain/board/api/BoardController.java
+++ src/main/java/com/backend7/frameworkstudy/domain/board/api/BoardController.java
@@ -6,8 +6,9 @@ import com.backend7.frameworkstudy.domain.board.dto.response.BoardResponse;
 import com.backend7.frameworkstudy.domain.board.dto.request.BoardUpdateRequest;
 import com.backend7.frameworkstudy.domain.board.service.BoardService;
 import com.backend7.frameworkstudy.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,33 +16,37 @@ import java.util.List;
 
 import static com.backend7.frameworkstudy.domain.board.exception.enumeration.BoardResultType.*;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
+@Tag(name = "게시판 API", description = "게시판 조회, 작성, 수정, 삭제")
 public class BoardController {
 
     private final BoardService boardService;
 
     @PostMapping("")
+    @Operation(summary = "게시글 작성")
     public ApiResponse<BoardResponse> createBoard(@RequestBody BoardCreateRequest request, @AuthenticationPrincipal MemberDetail memberDetail) {
         BoardResponse response = boardService.createBoard(memberDetail.getId(), request);
         return ApiResponse.ok(CREATE_BOARD, response);
     }
 
     @GetMapping("")
+    @Operation(summary = "게시글 목록 조회")
     public ApiResponse<List<BoardResponse>> findAll() {
         List<BoardResponse> response = boardService.findAll();
         return ApiResponse.ok(LOADED_BOARD_LIST, response);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 상세 조회")
     public ApiResponse<BoardResponse> findBoard(@PathVariable("id") Long id) {
         BoardResponse response = boardService.findBoardBy(id);
         return ApiResponse.ok(LOADED_BOARD, response);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "게시글 수정")
     public ApiResponse<BoardResponse> editBoard(
             @PathVariable("id") Long boardId, @RequestBody BoardUpdateRequest request, @AuthenticationPrincipal MemberDetail memberDetail) {
         BoardResponse response = boardService.editBoard(memberDetail.getId(), boardId, request);
@@ -49,6 +54,7 @@ public class BoardController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제")
     public ApiResponse<Void> deleteBoard(
             @PathVariable("id") Long boardId, @AuthenticationPrincipal MemberDetail memberDetail) {
         boardService.deleteBoard(memberDetail.getId(), boardId);
--- src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
+++ src/main/java/com/backend7/frameworkstudy/domain/member/api/MemberController.java
@@ -6,6 +6,8 @@ import com.backend7.frameworkstudy.domain.member.dto.MemberCreateRequest;
 import com.backend7.frameworkstudy.domain.member.dto.MemberResponse;
 import com.backend7.frameworkstudy.domain.member.service.MemberService;
 import com.backend7.frameworkstudy.global.common.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,17 +18,20 @@ import static com.backend7.frameworkstudy.domain.member.exception.MemberResultTy
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@Tag(name = "회원 API", description = "회원가입 및 로그인 기능")
 public class MemberController {
 
     private final MemberService memberService;
     private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/sign-up")
+    @Operation(summary = "회원가입")
     public ApiResponse<MemberResponse> singUp(@Valid @RequestBody MemberCreateRequest request) {
         return ApiResponse.ok(SIGN_UP, memberService.signUp(request));
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인")
     public ApiResponse<MemberResponse> login(@RequestBody LoginRequest request, HttpServletResponse response) {
         MemberResponse memberResponse = memberService.loginUser(request);
 
